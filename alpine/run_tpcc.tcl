39c9e697a2c241b70ebf28f7cbba749360f3b0fe
Malte Bengtsson <120107769+Maltextrakt@users.noreply.github.com>
2025-03-26 11:11:49 +0100

    Create run_tpcc.tcl

---
+++ alpine/run_tpcc.tcl
@@ -0,0 +1,24 @@
+puts "TPC-C Benchmark on PostgreSQL (Alpine) in Docker Container"
+
+dbset db pg
+
+diset connection pg_host localhost
+diset connection pg_port 5433
+
+diset tpcc pg_user hammerdb
+diset tpcc pg_pass hammerpw
+diset tpcc pg_dbase tpcc
+diset tpcc pg_superuser hammerdb
+diset tpcc pg_superuserpass hammerpw
+diset tpcc pg_count_ware 5
+diset tpcc pg_num_vu 5
+
+buildschema
+
+vucreate
+vuset vu 1
+vurun
+waittocomplete
+
+puts "TPC-C Benchmark complete"
+exit
