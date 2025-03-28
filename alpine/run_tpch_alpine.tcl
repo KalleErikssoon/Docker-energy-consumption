911fbb2e57e3be5e791d82d6604973b7070c6fac
Malte Bengtsson <120107769+Maltextrakt@users.noreply.github.com>
2025-03-28 12:27:22 +0100

    Update run_tpch_alpine.tcl

--- alpine/run_tpch_alpine.tcl
+++ alpine/run_tpch_alpine.tcl
@@ -7,12 +7,16 @@ diset connection pg_host localhost
 diset connection pg_port 5433
 diset connection pg_user postgres
 diset connection pg_pass postgres
+
 diset tpch pg_dbase tpch
+diset tpch pg_tpch_superuser postgres
+diset tpch pg_tpch_superuserpass postgres
+diset tpch pg_tpch_user tpch
+diset tpch pg_tpch_pass tpch
 diset tpch pg_num_tpch_threads 1
 
-buildschema
-
 vudestroy
+buildschema
 vucreate
 vuset vu 1
 vurun
